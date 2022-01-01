c6b76c9828b40639864cdc7c10e87681c2afacbd
Doug Flynn <dougflynn@yeshealth.com>
2021-12-31 21:32:39 -0500

    Add a decent player layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,58 @@
 import type { NextPage } from "next";
 import { Navbar } from "../src/components/Navbar";
-import { Fragment } from "react";
 import Head from "next/head";
 
 const Home: NextPage = () => {
     return (
-        <Fragment>
+        <div
+            className={"is-flex is-flex-direction-column"}
+            style={{
+                height: "100vh",
+                width: "100vw",
+            }}
+        >
             <Head>
                 <title>Home - SeasideFM Archive</title>
             </Head>
-            <Navbar />
-            <video controls autoPlay height={620} width={480}>
-                <source src={"/api/stream"} type={"video/mp4"} />
-            </video>
-        </Fragment>
+            <div className="container">
+                <Navbar />
+                <div className="columns mt-2">
+                    <div className="column">
+                        <video
+                            preload={"auto"}
+                            playsInline
+                            controls
+                            width={"100%"}
+                        >
+                            <source src={"/api/stream"} type={"video/mp4"} />
+                        </video>
+                    </div>
+                </div>
+            </div>
+            <footer className="footer mt-auto">
+                <div className="content has-text-centered">
+                    <p>
+                        <strong>Video content</strong> created under Fair Use
+                        conditions by{" "}
+                        <a href="https://twitch.tv/seasidefm">SeasideFM</a>.
+                        Content is licensed under Creative Commons except where
+                        not applicable by law.
+                        <br />
+                        <strong>Website</strong> programmed by{" "}
+                        <a href="https://github.com/DukeFerdinand">
+                            Doug Flynn
+                        </a>
+                        . The source code is licensed{" "}
+                        <strong>
+                            <a href="http://opensource.org/licenses/mit-license.php">
+                                MIT
+                            </a>
+                        </strong>
+                        .
+                    </p>
+                </div>
+            </footer>
+        </div>
     );
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 
 export const Navbar: React.FC = () => {
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
                 <a className="navbar-item" href="https://bulma.io">
-                    <Image src="https://bulma.io/images/bulma-logo.png" alt={"Logo"} width="112" height="28" />
+                    <Image
+                        src="https://bulma.io/images/bulma-logo.png"
+                        alt={"Logo"}
+                        width="112"
+                        height="28"
+                    />
                 </a>
 
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false"
-                   data-target="navbarBasicExample">
+                <a
+                    role="button"
+                    className="navbar-burger"
+                    aria-label="menu"
+                    aria-expanded="false"
+                    data-target="navbarBasicExample"
+                >
                     <span aria-hidden="true" />
                     <span aria-hidden="true" />
                     <span aria-hidden="true" />
@@ -19,33 +29,19 @@ export const Navbar: React.FC = () => {
 
             <div id="navbarBasicExample" className="navbar-menu">
                 <div className="navbar-start">
-                    <a className="navbar-item">
-                        Home
-                    </a>
+                    <a className="navbar-item">Home</a>
 
-                    <a className="navbar-item">
-                        Documentation
-                    </a>
+                    <a className="navbar-item">Documentation</a>
 
                     <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link">
-                            More
-                        </a>
+                        <a className="navbar-link">More</a>
 
                         <div className="navbar-dropdown">
-                            <a className="navbar-item">
-                                About
-                            </a>
-                            <a className="navbar-item">
-                                Jobs
-                            </a>
-                            <a className="navbar-item">
-                                Contact
-                            </a>
+                            <a className="navbar-item">About</a>
+                            <a className="navbar-item">Jobs</a>
+                            <a className="navbar-item">Contact</a>
                             <hr className="navbar-divider" />
-                                <a className="navbar-item">
-                                    Report an issue
-                                </a>
+                            <a className="navbar-item">Report an issue</a>
                         </div>
                     </div>
                 </div>
@@ -53,16 +49,23 @@ export const Navbar: React.FC = () => {
                 <div className="navbar-end">
                     <div className="navbar-item">
                         <div className="buttons">
-                            <a className="button is-primary">
-                                <strong>Sign up</strong>
-                            </a>
-                            <a className="button is-light">
-                                Log in
+                            <a
+                                href={`https://id.twitch.tv/oauth2/authorize
+    ?client_id=<your client ID>
+    &redirect_uri=<your registered redirect URI>
+    &response_type=code
+    &scope=user:read:subscriptions%20user:read:email`}
+                                style={{
+                                    backgroundColor: "#6441a5",
+                                }}
+                                className="button is-dark"
+                            >
+                                <strong>Log in with Twitch</strong>
                             </a>
                         </div>
                     </div>
                 </div>
             </div>
         </nav>
-    )
-}
+    );
+};
