3697e571010fe5c6ca5d6ed652980b0b5f7e089e
Doug Flynn <dougflynn@yeshealth.com>
2022-01-12 20:13:04 -0500

    Add GDPR screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { UserContextProvider } from "../src/context/UserContext";
+import { GDPRModalProvider } from "../src/context/GDPRContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
     const queryClient = new QueryClient();
     return (
-        <QueryClientProvider client={queryClient}>
-            <UserContextProvider>
-                <Component {...pageProps} />
-            </UserContextProvider>
-        </QueryClientProvider>
+        <GDPRModalProvider>
+            <QueryClientProvider client={queryClient}>
+                <UserContextProvider>
+                    <Component {...pageProps} />
+                </UserContextProvider>
+            </QueryClientProvider>
+        </GDPRModalProvider>
     );
 }
 
